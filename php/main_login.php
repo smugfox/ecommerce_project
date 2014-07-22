5aedd8041e8086b7ac5714403b43c5d2d35df1e4
smugfox <robinfoxin@gmail.com>
2014-07-22 00:10:27 +0000

    begun styling the forms

--- php/main_login.php
+++ php/main_login.php
@@ -1,35 +1,78 @@
 <?php
 
-include_once('jcart/jcart.php');
+include_once('jcart-1.3/jcart/jcart.php');
 
 session_start();
 ?>
 
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-<tr>
-<form name="form1" method="post" action="checklogin.php">
-<td>
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-<tr>
-<td colspan="3"><strong>Member Login </strong></td>
-</tr>
-<tr>
-<td width="78">Email Address</td>
-<td width="6">:</td>
-<td width="294"><input name="email" type="text" id="email"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td>:</td>
-<td><input name="password" type="text" id="password"></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td><input type="submit" name="Submit" value="Login"></td>
-</tr>
-</table>
-</td>
-</form>
-</tr>
-</table>
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Eshop</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css" />
+    <link rel="stylesheet" type="text/css" media="screen, projection" href="jcart-1.3/jcart/css/jcart.css" />
+    <script type="text/javascript" src="jcart-1.3/jcart/js/jcart.min.js"></script>
+    <script type="text/javascript" src="jcart-1.3/jcart/js/jquery-1.4.4.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="../js/scripts.js"></script>
+    
+    </head>
+    
+    <body>
+    <!--CONTAINER-->
+        <div id="container"> 
+    <!--CART-->
+    <div id="sidebar">
+        <div id="jcart"><?php $jcart->display_cart();?></div>
+    </div>
+    <!--END OF CART-->
+    <!--HEADER-->
+          <div id=header>
+            <div class="header_cart">
+           
+                <div id="logo">
+                    <a href="index.php"><img src="../img/logo.png"></a>
+                </div>
+        <!--END LOGO-->
+            </div>
+          </div>
+          
+          <div id="nav">
+              <!--SEARCH-->
+              
+              <form method="POST" action="results.php">
+                  <input type="text" name="search" class="field" id="q" placeholder="Search Store">
+                  <input type="image" value="Submit" name="submit" id="go" src="../img/icon_search.png">
+                  
+              </form>
+              <!--END SEARCH-->
+    <!--NAV-->
+        <?php include'nav.php';?>
+        </div>
+    <!--END OF NAV-->
+            <!--MAIN-->
+          
+            <div id ="reg_form">
+              <h2>Members Login</h2>
+              <form name="input" method="post" action="checklogin.php" >
+              <input type="text" name="email" class="reg" placeholder="Email Address"><br>
+              <input type="password" name="password" class="reg" placeholder="Password"><br>
+                <p class="action_bottom">
+                    <input type="submit" value="Login" class="button">
+                    or
+                    <a href="index.php" class="return_store">return to store</a>
+                </p>
+              </form>
+            </div>
+            <!--END MAIN-->
+
+
+              <!--FOOTER-->
+              <?php include'footer.php'?>
+              <!--END OF FOOTER-->
+        </div>
+        <!--END OF CONTAINER-->
+    </body>
+</html>
+
--- php/registration.php
+++ php/registration.php
@@ -1,29 +1,84 @@
 <?php
 
-include_once('jcart/jcart.php');
+    include_once('jcart-1.3/jcart/jcart.php');
 
 session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-<title>Eshop</title>
-</head>
-<body>
+    <head>
+    <title>Eshop</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css" />
+    <link rel="stylesheet" type="text/css" media="screen, projection" href="jcart-1.3/jcart/css/jcart.css" />
+    <script type="text/javascript" src="jcart-1.3/jcart/js/jcart.min.js"></script>
+    <script type="text/javascript" src="jcart-1.3/jcart/js/jquery-1.4.4.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="../js/scripts.js"></script>
+    
+    </head>
+    
+    <body>
+    <!--CONTAINER-->
+        <div id="container"> 
+    <!--CART-->
+    <div id="sidebar">
+        <div id="jcart"><?php $jcart->display_cart();?></div>
+    </div>
+    <!--END OF CART-->
+    <!--HEADER-->
+          <div id=header>
+            <div class="header_cart">
+           
+                <div id="logo">
+                    <a href="index.php"><img src="../img/logo.png"></a>
+                </div>
+        <!--END LOGO-->
+            </div>
+          </div>
+          
+          <div id="nav">
+              <!--SEARCH-->
+              
+              <form method="POST" action="results.php">
+                  <input type="text" name="search" class="field" id="q" placeholder="Search Store">
+                  <input type="image" value="Submit" name="submit" id="go" src="../img/icon_search.png">
+                  
+              </form>
+              <!--END SEARCH-->
+    <!--NAV-->
+        <?php include'nav.php';?>
+        </div>
+    <!--END OF NAV-->
+            <!--MAIN-->
+          
+            <div id ="reg_form">
+              <h2>REGISTRATION</h2>
+              <form name="input" action="add_customer.php" method="post">
+                <input type="text" name="firstname" class="reg" placeholder="First name"><br>
+                </p> <input type="text" name="lastname" class="reg" placeholder="Last name"><br>
+                </p> <input type="text" name="address" class="reg" placeholder="Address"><br>
+                <input type="text" name="city" class="reg" placeholder="City"><br>
+                <input type="text" name="state" class="reg" placeholder="State"><br>
+                <input type="text" name="zip" class="reg" placeholder="Zip"><br>
+                <input type="text" name="email" class="reg" placeholder="Email Address"><br>
+                <input type="password" name="password" class="reg" placeholder="Password"><br>
+                <p class="action_bottom">
+                    <input type="submit" value="Sign Up" class="button">
+                    or
+                    <a href="index.php" class="return_store">return to store</a>
+                </p>
+              </form>
+            </div>
+            <!--END MAIN-->
 
-<form name="input" action="add_customer.php" method="post">
-First name: <input type="text" name="firstname"><br>
-Last name: <input type="text" name="lastname"><br>
-Address: <input type="text" name="address"><br>
-City: <input type="text" name="city"><br>
-State: <input type="text" name="state"><br>
-Zip: <input type="text" name="zip"><br>
-Email Address: <input type="text" name="email"><br>
-Password: <input type="password" name="password"><br>
-<input type="submit" value="Submit">
-</form>
 
-</body>
-
+              <!--FOOTER-->
+              <?php include'footer.php'?>
+              <!--END OF FOOTER-->
+        </div>
+        <!--END OF CONTAINER-->
+    </body>
 </html>
+
